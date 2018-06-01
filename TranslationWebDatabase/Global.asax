7728e737833bf04ef39b50044fc51a6bcf8bb40b
Danny Bentley <danny.bentley@som.com>
2018-06-01 15:17:20 -0700

    MongoDB on Web setup

---
+++ TranslationWebDatabase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranslationWebDatabase.MvcApplication" Language="C#" %>
---
+++ TranslationWebDatabase/MongoContext.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TranslationWebDatabase
+{
+    public class MongoContext
+    {
+        MongoClient _client;
+        public IMongoDatabase _server;
+
+        public MongoContext()
+        {
+            //Reading credentials from Web.config file
+            var MongoDatabaseName = ConfigurationManager.AppSettings["MongoDatabaseName"];
+            var MongoUsername = ConfigurationManager.AppSettings["MongoUsername"];  
+            var MongoPassword = ConfigurationManager.AppSettings["MongoPassword"];  
+            var MongoPort = ConfigurationManager.AppSettings["MongoPort"];  //27017  
+            var MongoHost = ConfigurationManager.AppSettings["MongoHost"];
+
+            //Creating credentials 
+            var credential = MongoCredential.CreateMongoCRCredential(
+                MongoDatabaseName, MongoUsername, MongoPassword);
+
+            //Creating MongoClientSettings
+            var settings = new MongoClientSettings
+            {
+                Credentials = new[] {credential},
+                Server = new MongoServerAddress(MongoHost, Convert.ToInt32(MongoPort))
+            };
+
+            _client = new MongoClient(settings);
+            _server = _client.get();
+            _database = _server.GetDatabase(MongoDatabaseName);
+        }
+    }
+}
